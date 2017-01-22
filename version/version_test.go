6001ae17d4a70ab04114f3cdeab7a68b4a25d361
Haruaki OTAKE <aaharu@hotmail.com>
2017-01-23 02:37:13 +0900

    0.4.0
    fix version

--- version/version_test.go
+++ version/version_test.go
@@ -8,7 +8,7 @@ import "testing"
 
 func TestString(t *testing.T) {
 	actual := String()
-	expected := "schemarshal 0.3.0"
+	expected := "schemarshal 0.4.0"
 	if actual != expected {
 		t.Errorf("got %v\nwant %v", actual, expected)
 	}
